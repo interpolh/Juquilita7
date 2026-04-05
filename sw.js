efc4339625b8dce4e6d562a5c90c97e40532a9cc
interpolh <polhinfinito@gmail.com>
2026-04-04 23:04:42 -0600

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,22 @@
+const CACHE_NAME = "juquilita-app-v1";
+
+self.addEventListener("install", e => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll([
+        "./",
+        "./index.html",
+        "./dashboard.html",
+        "./index2.html"
+      ]);
+    })
+  );
+});
+
+self.addEventListener("fetch", e => {
+  e.respondWith(
+    caches.match(e.request).then(response => {
+      return response || fetch(e.request);
+    })
+  );
+});
