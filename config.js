a92515601a4f9dd2eb10e4874193258b29d42129
interpolh <polhinfinito@gmail.com>
2026-09-20 05:31:52 -0600

    Add files via upload

---
+++ config.js
@@ -0,0 +1,12 @@
+// Configuración general de Juquilita.
+// Para cambiar el nombre de la cooperación de reserva, modifica SOLO esta línea:
+window.COOPERACION_X_NOMBRE = "Cooperación X";
+window.JUQUILITA_META = 661154.46;
+window.JUQUILITA_FIREBASE_CONFIG = {
+  apiKey: "AIzaSyBboR7L9QYBF0VMI6kd4EahImEDnIbiV-U",
+  authDomain: "cooperaciones-juquilita.firebaseapp.com",
+  projectId: "cooperaciones-juquilita",
+  storageBucket: "cooperaciones-juquilita.firebasestorage.app",
+  messagingSenderId: "836131757319",
+  appId: "1:836131757319:web:70f93a56ccf152062bf9a2"
+};
--- sw.js
+++ sw.js
@@ -1,27 +1,5 @@
-const CACHE_NAME = "juquilita-v2";
-
-self.addEventListener("install", e => {
-  e.waitUntil(
-    caches.open(CACHE_NAME).then(cache => {
-      return cache.addAll([
-        "./",
-        "./index.html",
-        "./dashboard.html",
-        "./index2.html"
-      ]);
-    })
-  );
-});
-
-self.addEventListener("fetch", e => {
-  e.respondWith(
-    fetch(e.request)
-      .then(res => {
-        return caches.open(CACHE_NAME).then(cache => {
-          cache.put(e.request, res.clone());
-          return res;
-        });
-      })
-      .catch(() => caches.match(e.request))
-  );
-});
+const CACHE_NAME = "juquilita-v4";
+const ARCHIVOS = ["./","./index.html","./Cooperaciones.html","./Proyecto.html","./Depositos.html","./gastos.html","./index2.html","./juquilita.css","./config.js","./manifest.json","./icono.png"];
+self.addEventListener("install",e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE_NAME).then(c=>c.addAll(ARCHIVOS)))});
+self.addEventListener("activate",e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE_NAME).map(k=>caches.delete(k)))).then(()=>self.clients.claim()))});
+self.addEventListener("fetch",e=>{if(e.request.method!=="GET")return;e.respondWith(fetch(e.request).then(res=>{const copia=res.clone();caches.open(CACHE_NAME).then(c=>c.put(e.request,copia));return res}).catch(()=>caches.match(e.request)))});
